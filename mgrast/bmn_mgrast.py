80242694fecc972da9c5d83c5eebe42fa08fa8de
Manu Prestat <emmanuel.prestat.pro@gmail.com>
2013-12-13 16:25:58 +0100

    modified:   mgrast/bmn_mgrast.py

--- mgrast/bmn_mgrast.py
+++ mgrast/bmn_mgrast.py
@@ -11,12 +11,15 @@ import os, sys
 parser = argparse.ArgumentParser(description="This is the bioman MG-RAST toolkit. It can show which projects have a chosen pattern in metadata, tell more about it, and download metagenomes directly once mgm ID provided")
 parser.add_argument("pattern", help="a pattern to scan or a mgm ID for downloading")
 parser.add_argument("-t", "--task", default = "download", choices=['download', 'scan', 'details'], help="Choose the task: you want to download, scan or ask for details about a MG-RAST project? Default is download")
-parser.add_argument("-s", "--stage", default = "upload", choices=['upload', 'consensus', 'drisee', 'kmer', 'preprocess', 'dereplication', 'screen', 'genecalling', 'search', 'cluster', 'loadAWE', 'superblat', 'abundance', 'loadDB', 'done', 'all'], help="What stage of analysis should be downloaded? 'all' means everything")
+parser.add_argument("-s", "--stage", default = "upload", choices=['upload', 'consensus', 'drisee', 'kmer', 'preprocess', 'dereplication', 'screen', 'genecalling', 'search', 'cluster', 'loadAWE', 'superblat', 'abundance', 'loadDB', 'done', 'all'], help="What stage of analysis should be downloaded? 'all' means everything. Default is 'upload' meaning data uploaded by the MG-RAST user.")
+parser.add_argument("-v", "--verbosity", default = "mixs", choices=['minimal', 'mixs', 'metadata', 'stats', 'full'], help="Level of verbosity associated to task 'scan'. The default 'mixs' is advised to get a project id necessary to the task 'details'.")
 args = parser.parse_args()
 
 p = args.pattern
 t = args.task
 s = args.stage
+v = args.verbosity
+
 mgrast = "http://api.metagenomics.anl.gov/1/"
 
 def main():
@@ -58,7 +61,6 @@ def download(url, fileName=None, path="mgdata"):
 def downloadMG(jid, stage="upload"):
     """
     download data
-    file_type":"stats","stage_name":"done","file_name":"999.done.order.stats","url":"http://api.metagenomics.anl.gov/1/download/mgm4447943.3?file=999.9","file_id":"999.9","id":"mgm4447943.3","stage_id":"999"
     """
     if jid.startswith("mgm"):
 	jid=jid[3:]
@@ -70,16 +72,18 @@ def downloadMG(jid, stage="upload"):
     for rec in files:
         MGRec[rec['file_name']]=rec['url']
         jidpath='mgm'+jid+'/'+'mgm'+jid+"-"+stage
-        print "file", rec['file_name'], "from project mgm'" + jid, "'tagged as '" + stage + "', is being downloaded under directory" + jidpath + " using this url: " + rec['url']
+        print "file", rec['file_name'], "from project mgm" + jid, "tagged as '" + stage + "', is being downloaded under directory " + jidpath + " using this url: " + rec['url']
         download(rec['url'],path=jidpath)
     return MGRec
 
 def projectDetails(jid, verb="full"):
     """
     get project details
-    fetch info from http://api.metagenomics.anl.gov/1/project/mgp128?verbosity=full
-    
     """
+    if jid.startswith("mgm"):
+        raise NameError("You've provided a 'metagenome id' whereas a 'project id' was expected")
+    elif jid.startswith("mgp"):
+        jid=jid[3:]
     q = mgrast + "project/mgp" + jid + "?verbosity=" + verb
     try:
         pdetails = json.load(urllib2.urlopen(q))
@@ -98,15 +102,23 @@ def projectDetails(jid, verb="full"):
             print element
             for i,j in detail:
                 print '\t' + i + ':\t' + j
+        elif element=='libraries':
+            print element
+            for i,j in detail:
+                print '\t' + i + ':\t' + j
+        elif element=='samples':
+            print element
+            for i,j in detail:
+                print '\t' + i + ':\t' + j
         else: print str(element) + ':\t' + str(detail)
     
-def scanMGtab(pattern):
+def scanMGtab(pattern, verb=v):
     """
     scans all metadata associated to all MG-RAST projects
     for any keyword, and outputs project identifiers
     results are shown as a table
     """
-    q = mgrast + "metagenome?metadata=" + pattern + "&verbosity=minimal&limit=1000&order=id&direction=asc&match=all&status=both"
+    q = mgrast + "metagenome?metadata=" + pattern + "&verbosity="+ verb + "&limit=1000&order=id&direction=asc&match=all&status=both"
     try:
         p = json.load(urllib2.urlopen(q))
     except urllib2.HTTPError, err:
@@ -117,6 +129,11 @@ def scanMGtab(pattern):
             raise NameError('Query was not successful')
     np=""
     MGdata=[]
+    header=""
+    if verb=="mixs":
+        header="status\tfeature\tmaterial\tproject_name\tname\tseq_method\tcollection_date\tcountry\tcreated\tbiome\tlongitude\tsequence_type\turl\tlocation\tPI_lastname\tenv_package_type\tlatitude\tproject_id\tid\tPI_firstname"
+    elif verb=="minimal":
+        header="status\tid\tname\tcreated"
     for element, detail in p.iteritems():
         if element=="data":
             for i in detail:
@@ -129,9 +146,9 @@ def scanMGtab(pattern):
     if np>0:
         """Make a fancy output"""
         print
-        print "status\tid\tname\tcreated"
+        print header
         for project in MGdata:
-            print '\t'.join(project)
+            print '\t'.join(str(x) for x in project)
         print
         print "--------"
         print "A total of " + str(np) + " metagenome projects were found."
