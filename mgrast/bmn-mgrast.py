ed6581729404468f76f3400f2adc9afd7dacc3bd
Manu Prestat <emmanuel.prestat.pro@gmail.com>
2013-12-31 12:29:04 +0100

    modified:   mgrast/bmn-mgrast.py Added the outfile argument

--- mgrast/bmn-mgrast.py
+++ mgrast/bmn-mgrast.py
@@ -8,17 +8,22 @@ import shutil
 import urlparse
 import os, sys
 
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 parser = argparse.ArgumentParser(description="This is the bioman MG-RAST toolkit. It can show which projects have a chosen pattern in metadata, tell more about it, and download metagenomes directly once mgm ID provided")
 parser.add_argument("pattern", help="a pattern to scan or a mgm ID for downloading")
-parser.add_argument("-t", "--task", default = "download", choices=['download', 'scan', 'details'], help="Choose the task: you want to download, scan or ask for details about a MG-RAST project? Default is download")
+parser.add_argument("-t", "--task", required=True, default = "download", choices=['download', 'scan', 'details'], help="Choose the task: you want to download, scan or ask for details about a MG-RAST project? Default is download")
 parser.add_argument("-s", "--stage", default = "upload", choices=['upload', 'consensus', 'drisee', 'kmer', 'preprocess', 'dereplication', 'screen', 'genecalling', 'search', 'cluster', 'loadAWE', 'superblat', 'abundance', 'loadDB', 'done', 'all'], help="What stage of analysis should be downloaded? 'all' means everything. Default is 'upload' meaning data uploaded by the MG-RAST user.")
 parser.add_argument("-v", "--verbosity", default = "mixs", choices=['minimal', 'mixs', 'metadata', 'stats', 'full'], help="Level of verbosity associated to task 'scan'. The default 'mixs' is advised to get a project id necessary to the task 'details'.")
+parser.add_argument("-so", "--scanoutput", help="Scan output filename. Standard output if not given")
 args = parser.parse_args()
 
 p = args.pattern
 t = args.task
 s = args.stage
 v = args.verbosity
+o = args.scanoutput
 
 mgrast = "http://api.metagenomics.anl.gov/1/"
 
@@ -112,7 +117,7 @@ def projectDetails(jid, verb="full"):
                 print '\t' + i + ':\t' + j
         else: print unicode(element) + ':\t' + unicode(detail)
     
-def scanMGtab(pattern, verb=v):
+def scanMGtab(pattern, verb=v, outfile=o):
     """
     scans all metadata associated to all MG-RAST projects
     for any keyword, and outputs project identifiers
@@ -144,17 +149,26 @@ def scanMGtab(pattern, verb=v):
         elif element=="total_count":
                 np = detail
     if np>0:
-        """Make a fancy output"""
-        print
-        print header
-        for project in MGdata:
-            print '\t'.join(unicode(x) for x in project)
+        if not outfile:
+            """Make a fancy output"""
+            print
+            print header
+            for project in MGdata:
+                print '\t'.join(unicode(x) for x in project)
+        else:
+            with open(outfile, 'w') as out:
+                out.write(header + '\n')
+                for project in MGdata:
+                    out.write('\t'.join(unicode(x) for x in project) + '\n')
         print
         print "--------"
-        print "A total of " + unicode(np) + " metagenome projects were found."
+        print "A total of " + unicode(np) + " metagenome(s) were found."
         print "--------"
+        if outfile:
+            print "File '" + outfile + "' written."
     else:
         print "No project was found, come back tomorrow."
+               
 
 # an older version with a less fancy output than scanMGtab()
 def scanMG(pattern):
